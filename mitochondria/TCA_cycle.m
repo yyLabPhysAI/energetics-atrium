3c4f41135e7b9567b6012518d6ab6a082af65d40
Noam Keidar <noam@sibylla.io>
2022-02-07 12:26:49 +0200

    Mito is reactive but small amplitude

--- mitochondria/TCA_cycle.m
+++ mitochondria/TCA_cycle.m
@@ -24,51 +24,53 @@ K_D_Ca=data.K_D_Ca;
 
 K_D_Mg=data.K_D_Mg;
 
+k = 1/4;
+
 % Citrate
 C_CIT = C_K_int - (C_ISOC + C_aKG + C_SCoA + C_Suc + C_FUM + C_MAL + C_OAA);
 
 % Citrate synthase (CS)
-V_CS = k_cat_cs.*E_T_cs.*(1 + (K_M_AcCoA./C_AcCoA) + (K_M_OAA./C_OAA) + (K_M_AcCoA./C_AcCoA) + (K_M_OAA./C_OAA)).^(-1);
+V_CS = k*k_cat_cs.*E_T_cs.*(1 + (K_M_AcCoA./C_AcCoA) + (K_M_OAA./C_OAA) + (K_M_AcCoA./C_AcCoA) + (K_M_OAA./C_OAA)).^(-1);
 
 % Aspartate amino transferase (AAT)
 V_AAT = 0; %k_f_AAT.*C_OAA.*C_GLU.*(k_ASP.*K_E_AAT)./(k_ASP.*K_E_AAT + C_aKG.*k_f_AAT);
 
 % Aconitase (ACO)
-V_ACO = 69.5704*k_f_ACO.*(C_CIT - (C_ISOC./k_E_ACO));
+V_ACO = k*69.5704*k_f_ACO.*(C_CIT - (C_ISOC./k_E_ACO));
 
 % Isocitrate dehydrogenase (IDH)
 Ni = 2;
 
 f_a_IDH = ((1 + (C_ADP_m./K_ADP_a)).*(1 + (Ca_m./K_Ca_a))).^(-1);
 f_i_IDH = 1 + (C_NADH./K_i_NADH);
-V_IDH = 73.1778*k_cat_IDH.*E_T_IDH.*(1 + (C_H./k_h_1) + (k_h_2./C_H) + f_i_IDH.*(K_M_NAD./C_NAD) + f_a_IDH.*(K_M_ISOC./C_ISOC).^Ni + f_a_IDH.*f_i_IDH.*(K_M_ISOC./C_ISOC).^Ni.*(K_M_NAD./C_NAD)).^-1;
+V_IDH = k*73.1778*k_cat_IDH.*E_T_IDH.*(1 + (C_H./k_h_1) + (k_h_2./C_H) + f_i_IDH.*(K_M_NAD./C_NAD) + f_a_IDH.*(K_M_ISOC./C_ISOC).^Ni + f_a_IDH.*f_i_IDH.*(K_M_ISOC./C_ISOC).^Ni.*(K_M_NAD./C_NAD)).^-1;
 
 dC_ISOC = V_ACO - V_IDH;
 
 % Alpha-ketoglutarate dehydrogenase (KGDH)
 f_a_KGDH = ((1 + (C_Mg./K_D_Mg)).*(1 + (Ca_m./K_D_Ca))).^(-1);
-V_KGDH = 0.9101*(k_cat_KGDH.*E_T_KGDH)./(1 + f_a_KGDH.*(K_M_aKG./C_aKG).^(n_aKG)+f_a_KGDH.*((K_M_NAD_new)./C_NAD)); % ?????? why not K_M_NAD??
+V_KGDH = k*0.9101*(k_cat_KGDH.*E_T_KGDH)./(1 + f_a_KGDH.*(K_M_aKG./C_aKG).^(n_aKG)+f_a_KGDH.*((K_M_NAD_new)./C_NAD)); % ?????? why not K_M_NAD??
 
 dC_aKG = V_IDH - V_KGDH + V_AAT;
 
 % Succinyl CoA lyase (SL)
-V_SL = 22.1834*k_f_SL.*((C_SCoA.*C_ADP_m - (C_Suc.*C_ATP_m.*C_CoA)./(k_E_SL)));
+V_SL = k*22.1834*k_f_SL.*((C_SCoA.*C_ADP_m - (C_Suc.*C_ATP_m.*C_CoA)./(k_E_SL)));
 dC_SCoA = V_KGDH - V_SL;
 
 % Succinate dehydrogenase (SDH)
-V_SDH = 659.7138*(k_cat_SDH.*E_T_SDH)./...
+V_SDH = k*659.7138*(k_cat_SDH.*E_T_SDH)./...
     (1 + (K_M_SUC./C_Suc).*(1 + C_OAA./K_i_sdh_OAA).*(1 + C_FUM./K_i_FUM));
 
 dC_Suc = V_SL - V_SDH;
 
 % Fumarate hydratase (FH)
-V_FH = 22.1834*k_f_FH.*(C_FUM - C_MAL./(K_E_FH));
+V_FH = k*22.1834*k_f_FH.*(C_FUM - C_MAL./(K_E_FH));
 dC_FUM = V_SDH - V_FH;
 
 % Malate dehydrogenase (MDH)
 f_h_a = (1 + C_H./k_h1 + C_H.^2./(k_h1.*k_h2)).^-1 + k_offset;
 f_h_i = (1 + k_h3./C_H + (k_h3.*k_h4)./C_H.^2).^-2;
-V_MDH = 71.7008*(k_cat_MDH.*E_T_MDH.*f_h_a.*f_h_i)./(1 + (K_M_MAL./C_MAL).*(1 + (C_OAA./K_i_OAA)) + (K_M_NAD_mdh./C_NAD) + ((K_M_MAL./C_MAL).*(1 + (C_OAA./K_i_OAA)).*(K_M_NAD_mdh))./(C_NAD)); % ???????? Why different K_M_NAD_mdh????
+V_MDH = k*71.7008*(k_cat_MDH.*E_T_MDH.*f_h_a.*f_h_i)./(1 + (K_M_MAL./C_MAL).*(1 + (C_OAA./K_i_OAA)) + (K_M_NAD_mdh./C_NAD) + ((K_M_MAL./C_MAL).*(1 + (C_OAA./K_i_OAA)).*(K_M_NAD_mdh))./(C_NAD)); % ???????? Why different K_M_NAD_mdh????
 
 dC_MAL = V_FH - V_MDH;
 
@@ -78,9 +80,9 @@ dC_OAA = V_MDH - V_CS - V_AAT;
 
 % PDH
 K_CaAcCoa = 0.2e-4; % mM
-K_PDH = 0.37; % muM/s
+K_PDH = 0.37/28; % muM/s
 C_PDH =  0.12; % mM
-dC_AcCoA = K_PDH*C_PDH*(Ca_m./(Ca_m + K_CaAcCoa)) - 6.6754*V_CS;
+dC_AcCoA = K_PDH*C_PDH*(Ca_m./(Ca_m + K_CaAcCoa)) - V_CS;
 
 
 end
--- mitochondria/oxidative_phosphorylation.m
+++ mitochondria/oxidative_phosphorylation.m
@@ -2,7 +2,7 @@ function [...
     V_He, dC_FLV, V_He_F, dC_NADH, V_Hu, V_H_Leak, dC_ADP_m, V_ANT, V_O2]...
     = oxidative_phosphorylation(V_SL, V_IDH, V_KGDH, V_MDH, V_SDH, ...
     delta_Psi_m, C_NADH, C_NAD, C_ATP_m, C_ADP_m, Ca_m, ATP_i, ...
-    ADP_i, data)
+    ADP_i, dC_AcCoA,  data)
 
 r_a = data.r_a;
 r_c1 = data.r_c1;
@@ -37,10 +37,10 @@ T = data.T;
 F = data.F;
 
 % The respiration-driven proton pump
-g = 1/2000; % voltage correction factor
+g = 1/2; % voltage correction factor
 delta_mu_h = -2.303.*(R.*T./F).*delta_pH + delta_Psi_m;
 A_res = (R.*T./F).*log(K_res.*sqrt(C_NADH./C_NAD));
-V_O2 = 0.5.*rho_res.*((r_a + r_c1.*exp(6.*F.*psi_B./(R.*T))).*exp(A_res.*F./(R.*T)) - r_a.*exp((g.*6.*F.*delta_mu_h)./(R.*T)) + r_c2.*exp(A_res.*F./(R.*T)).*exp(g.*6.*F.*delta_mu_h./(R.*T)))./...
+V_O2 = 9.1979*0.2515*7.0810*0.5.*rho_res.*((r_a + r_c1.*exp(6.*F.*psi_B./(R.*T))).*exp(A_res.*F./(R.*T)) - r_a.*exp((g.*6.*F.*delta_mu_h)./(R.*T)) + r_c2.*exp(A_res.*F./(R.*T)).*exp(g.*6.*F.*delta_mu_h./(R.*T)))./...
                    ((1 + r_1.*exp(A_res.*F./(R.*T))) .* exp(6.*F.*psi_B./(R.*T)) + (r_2 + r_3.*exp(A_res.*F./(R.*T))).*exp(g.*6.*F.*delta_mu_h./(R.*T)));
 V_He = 6.*rho_res.*(r_a.*exp(A_res.*F./(R.*T)) - (r_a + r_b).*exp(g.*6.*F.*delta_mu_h./(R.*T)))./...
                  ((1 + r_1.*exp(A_res.*F./(R.*T))) .* exp(6.*F.*psi_B./(R.*T)) + (r_2 + r_3.*exp(A_res.*F./(R.*T))).*exp(g.*6.*F.*delta_mu_h./(R.*T)));
@@ -48,7 +48,7 @@ dC_FLV = V_SDH - V_O2;
 A_res_F = (R.*T./F).*log(K_resF.*sqrt(FADH2./FADH)); % 
 V_He_F = 4.*rho_resF.*(r_a.*exp(A_res_F.*F./(R.*T)) - (r_a + r_b).*exp(g.*6.*F.*delta_mu_h./(R.*T)))./...
                     ((1 + r_1.*exp(A_res_F.*F./(R.*T))).*exp(6.*F.*psi_B./(R.*T)) + (r_2 + r_3.*exp(A_res_F.*F./(R.*T))).*exp(g.*6.*F.*delta_mu_h./(R.*T)));
-dC_NADH = -V_O2 + V_IDH + V_KGDH + V_MDH;
+dC_NADH = -V_O2 + V_IDH + V_KGDH + V_MDH + dC_AcCoA;
 
 % The F_1F_0-ATPase
 A_F1 = (R.*T./F).*log(K_F1.*C_ATP_m./(C_ADP_m.*data.Pi));
@@ -59,7 +59,7 @@ V_Hu = -3.*rho_F1.*(10.^2.*p_a.*(1 + exp(A_F1./(R.*T))) - (p_a + p_b).*exp(3.*F.
 
 % Adenine nucleotide translocator (ANT) and proton leak
 V_H_Leak = g_H.*delta_mu_h;
-V_ANT = V_ant_max.*(0.75.*(1 - ((0.25.*ATP_i.*0.45.*C_ADP_m)./(0.17.*ADP_i.*0.025.*C_ATP_m))).*exp(-(F.*delta_Psi_m)./(R.*T)))./...
+V_ANT = V_ant_max.*(0.75.*(1 - 1e-4*((0.25.*ATP_i.*0.45.*C_ADP_m)./(0.17.*ADP_i.*0.025.*C_ATP_m))).*exp(-(F.*delta_Psi_m)./(R.*T)))./...
                   ((1 + ((0.25.*ATP_i)./(0.225.*ADP_i)).*exp((-h_ANT.*F.*delta_Psi_m)./(R.*T))).*(1 + ((0.45.*C_ADP_m)./(0.025.*C_ATP_m ))));
 dC_ADP_m = V_ANT - V_ATPase - V_SL;
 
